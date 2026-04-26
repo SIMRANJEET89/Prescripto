010ae57cea277eaebee78a37946022b5c22fcaf1
simranjeet kour <simranjeetkour8407@gmail.com>
2026-04-26 11:01:07 +0530

    fix mobile profile dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,12 +46,13 @@ const Navbar = () => {
           <div className="flex items-center gap-2 cursor-pointer group relative">
             <img className="w-8 rounded-full" src={userData.image} alt="" />
 
-            <img
+            <img onClick={() => setShowMenu(!showMenu)}
               className="w-2.5 invert brightness-0"
               src={assets.dropdown_icon}
               alt=""
             />
 
+          {showMenu && 
             <div className="absolute top-0 ring-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
@@ -71,6 +72,7 @@ const Navbar = () => {
                 </p>
               </div>
             </div>
+            }
           </div>
         ) : (
           <button
